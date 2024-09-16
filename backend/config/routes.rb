5ab92cb553a51a3f181ea97505e348b5cff20703
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-09-16 11:03:19 +0900

    routingを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,36 @@
 Rails.application.routes.draw do
+  # apiの基本rootは /api/v1
   namespace :api do
-    # APIのルートをここに定義
-    # 例：
-    root 'home#index'
-    resources :users
-    resources :posts
-    resources :tasks
+    namespace :v1 do
+      resources :quests, param: :uuid, except: %i[new show] do
+        delete :trashed
+        resources :courses, param: :uuid, only: %i[index update destroy]
+      end
+
+      resources :courses, param: :uuid, except: %i[new index show] do
+        delete :trashed
+        resources :stages, param: :uuid, only: %i[index update destroy]
+      end
+
+      resources :stages, param: :uuid, except: %i[new index show] do
+        delete :trashed
+        resources :questions, param: :uuid, only: %i[index update destroy]
+      end
+
+      resources :questions, param: :uuid, except: %i[new index show] do
+        delete :trashed
+        post :answer
+        resources :false_answers, param: :uuid, only: %i[create update destroy] do
+          delete :trashed
+        end
+        resources :words, param: :uuid, only: %i[index update destroy]
+      end
+
+      resources :words, param: :uuid, except: %i[new index show] do
+        delete :trashed
+      end
+
+    end
   end
+
 end
