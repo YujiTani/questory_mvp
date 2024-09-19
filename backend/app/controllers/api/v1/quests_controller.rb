7ce9fb42ac95477baa7b74f12dd37c24c8fefd94
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-09-20 01:00:08 +0900

    すべてのメソッド実行前にResponseIDを付与するように変更

--- backend/app/controllers/api/v1/quests_controller.rb
+++ backend/app/controllers/api/v1/quests_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::QuestsController < Api::V1::BaseController
+  # すべてのメソッドであらかじめrequest_idを生成する
+  before_action :set_request_id, only: [:index, :create, :update, :destroy, :trashed]
+
   def index
     all_quests = Quest.all.without_deleted
     # limit,offsetを受け取った場合はその値を使用する
@@ -10,18 +13,18 @@ class Api::V1::QuestsController < Api::V1::BaseController
 
     render json: {
       ok: true,
-      request_id: SecureRandom.uuid,
+      request_id: @request_id,
       quests: quests,
       total: all_quests.count,
       limit: limit,
       offset: offset,
-    }
+    }, status: :ok
 
   rescue => e
     logger.error "Error in QuestsController#index: #{e.message}"
     render json: {
       ok: false,
-      request_id: SecureRandom.uuid,
+      request_id: @request_id,
       code: "InternalServerError",
       message: "エラーの詳細メッセージ",
       errors: e.message
@@ -34,35 +37,34 @@ class Api::V1::QuestsController < Api::V1::BaseController
     if quest.save
       render json: {
         ok: true,
-        request_id: SecureRandom.uuid,
+        request_id: @request_id,
         quest: quest
-      }
+      }, status: :ok
     else
       render json: {
         ok: false,
-        request_id: SecureRandom.uuid,
+        request_id: @request_id,
         code: "ParameterError",
         message: "パラメーターエラー （必須パラメーターを渡さなかった・パラメーターのデータ型を間違えた等）",
-        errors: quest.errors
-      }
+        errors: quest.errors.full_messages
+      }, status: :bad_request
     end
   end
 
 
   def update
     quest = Quest.find_by(id: quest_params[:id])
-    request_id = SecureRandom.uuid
 
     if quest.update(quest_params)
       render json: {
         ok: true,
-        request_id: request_id,
+        request_id: @request_id,
         quest: quest
       }
     else
       render json: {
         ok: false,
-        request_id: request_id,
+        request_id: @request_id,
         code: "NotFound",
         message: "エラーの詳細メッセージ",
         errors: quest.errors
@@ -72,17 +74,16 @@ class Api::V1::QuestsController < Api::V1::BaseController
 
   def destroy
     quest = Quest.find_by(id: quest_params[:id])
-    request_id = SecureRandom.uuid
 
     if quest.soft_delete
       render json: {
         ok: true,
-        request_id: request_id,
+        request_id: @request_id,
       }
     else
       render json: {
         ok: false,
-        request_id: request_id,
+        request_id: @request_id,
         code: "NotFound",
         message: "エラーの詳細メッセージ",
         errors: quest.errors
@@ -92,17 +93,16 @@ class Api::V1::QuestsController < Api::V1::BaseController
 
   def trashed
     quest = Quest.find_by(id: quest_params[:id])
-    request_id = SecureRandom.uuid
 
     if quest.destroy
       render json: {
         ok: true,
-        request_id: request_id,
+        request_id: @request_id,
       }
     else
       render json: {
         ok: false,
-        request_id: request_id,
+        request_id: @request_id,
         code: "NotFound",
         message: "エラーの詳細メッセージ",
         errors: quest.errors
@@ -114,5 +114,9 @@ class Api::V1::QuestsController < Api::V1::BaseController
 
   def quest_params
     params.require(:quest).permit(:name, :description, :state)
+  end
+
+  def set_request_id
+    @request_id = SecureRandom.uuid
   end
 end
