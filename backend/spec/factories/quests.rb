3a88d8a1d16ed53949e38b48881388d3e64805ca
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-09-20 00:34:11 +0900

    factory_botでuuidとstateを設定しないように変更

--- backend/spec/factories/quests.rb
+++ backend/spec/factories/quests.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
   factory :quest do
-    uuid { SecureRandom.uuid }
     sequence(:name) { |n| "Quest #{n}" }
     description { "This is a test quest description" }
-    state { :draft }
 
     trait :published do
       state { :published }
