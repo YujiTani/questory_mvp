0114442f5d78ec4c6e5a8f472dd0c986d976327d
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-09-20 09:26:42 +0900

    グローバルにキャッチするエラーの種類を追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,9 +3,12 @@ class ApplicationController < ActionController::API
   before_action :basic_auth
   before_action :generate_response_id
 
-  rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
   rescue_from ArgumentError, with: :handle_argument_error
   rescue_from StandardError, with: :handle_standard_error
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
+  rescue_from ActiveRecord::StatementInvalid, with: :handle_statement_invalid
+
 
   # ログ追跡用のresponse_idを生成する
   def generate_response_id
@@ -21,17 +24,27 @@ class ApplicationController < ActionController::API
     end
   end
 
-  # レコードが無効な場合のエラーハンドリング
+  # レコードが見つからない場合のエラーハンドリング
+  def handle_record_not_found(exception)
+    render json: { ok: false, response_id: @response_id, errors: exception.message }, status: :not_found
+  end
+
+  # # レコードが無効な場合のエラーハンドリング
   def handle_record_invalid(exception)
     render json: { ok: false, response_id: @response_id, errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
-  # 引数が無効な場合のエラーハンドリング
+  # # 引数が無効な場合のエラーハンドリング
   def handle_argument_error(exception)
     render json: { ok: false, response_id: @response_id, message: exception.message }, status: :bad_request
   end
 
-  # 標準エラーのエラーハンドリング
+  # ステートメントが無効な場合のエラーハンドリング
+  def handle_statement_invalid(exception)
+    render json: { ok: false, response_id: @response_id, errors: exception.message }, status: :unprocessable_entity
+  end
+
+  # # 標準エラーのエラーハンドリング
   def handle_standard_error(exception)
     Rails.logger.error(exception.message)
     Rails.logger.error(exception.backtrace.join("\n"))
